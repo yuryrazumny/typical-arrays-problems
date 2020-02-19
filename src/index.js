80451515e438ebc00634228e30d277186fd5bc21
unknown <yury971@mail.ru>
2020-02-19 23:21:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array != undefined && array.length != 0) {
+        var minimal = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] < minimal) minimal = array[i];
+        }
+        return minimal;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array != undefined && array.length != 0) {
+        var maximal = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] > maximal) maximal = array[i];
+        }
+        return maximal;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array != undefined && array.length != 0) {
+        var count = 0;
+        var qty = array.length;
+        for (var i = 0; i < array.length; i++) {
+            count += array[i];
+        }
+        return count / qty;
+    }
+    return 0;
 }
